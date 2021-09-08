d8f414fb65c90124ca8b5bb3ef9e9907581cab55
J-382 <jrperezdeleon@gmail.com>
2021-09-08 15:56:02 -0500

    Fixed port issues

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -5,9 +5,6 @@ import java.nio.charset.Charset;
 import java.io.*;
 import java.util.ArrayList;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class HttpServer {
     private static final HttpServer _instance = new HttpServer();
     private static final String HTTP_MESSAGE = "HTTP/1.1 200 OK\n" + "Content-Type: #/#\r\n" + "\r\n";
@@ -19,10 +16,17 @@ public class HttpServer {
 
     private HttpServer(){}
 
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
     public void start(String[] args) throws IOException{
         ServerSocket serverSocket = null;
         try {
-            serverSocket = new ServerSocket(4567);
+            serverSocket = new ServerSocket(getPort());
         } catch (IOException e) {
             System.err.println("Could not listen on port: 35000.");
             System.exit(1);
@@ -84,10 +88,6 @@ public class HttpServer {
             ans = getWheaterJSON(city);
         }
         return ans;
-    }
-
-    public String computeHTMLResponse(String cityname){
-        return getDefaultPage();
     }
 
     public static String getWheaterJSON(String cityname) throws IOException{
